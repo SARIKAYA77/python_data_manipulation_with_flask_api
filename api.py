aa86535b1b41c6a77546611cf805734cd963dce7
SARIKAYA <mustafasarikaya1@hotmail.com>
2022-01-23 21:11:25 +0300

    test and json_data fix

--- api.py
+++ api.py
@@ -1,17 +1,19 @@
 import requests
-
-data = {
-    "10:00-11:00": [{"type": "in", "value": 15}, {"type": "out", "value": 12}, {"type": "in", "value": 4}],
-    "11:00-12:00": [{"type": "in", "value": 11}, {"type": "out", "value": 14}, {"type": "out", "value": 4}],
-    "12:00-13:00": [{"type": "in", "value": 24}, {"type": "out", "value": 4}, {"type": "in", "value": 15}],
-    "13:00-14:00": [{"type": "in", "value": 8}, {"type": "out", "value": 25}, {"type": "in", "value": 12}],
-    "14:00-15:00": [{"type": "in", "value": 3}, {"type": "out", "value": 5}, {"type": "out", "value": 17},
-                    {"type": "out", "value": 10}],
-    "15:00-16:00": [{"type": "in", "value": 19}, {"type": "out", "value": 2}],
-    "16:00-17:00": [],
-    "17:00-18:00": [{"type": "in", "value": 0}, {"type": "out", "value": 18}]
-}
+import json
 
 
-response = requests.post("http://127.0.0.1:5000/resource", json=data)
-print(response.text)
+def get_data_from_api():
+    with open('data.json') as f:
+        data = json.load(f)
+
+    response = requests.post("http://127.0.0.1:5000/resource", json=data)
+    return response
+
+
+def main():
+    response = get_data_from_api()
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,17 @@
+from app import app
+import api
+import unittest
+
+
+class FlaskTest(unittest.TestCase):
+    def runTest_update_text(self):
+        tester = app.test_client(self)
+        response = api.get_data_from_api()
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+        print("test case 1 completed")
+
+
+if __name__ == "__main__":
+    tester = FlaskTest()
+    tester.runTest_update_text()
